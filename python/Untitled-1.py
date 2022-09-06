3499c023392bf8ba29f0f96cd5fa1117c6c482f3
mücahit <mucahitceren@gmail.com>
2022-09-06 22:08:52 +0300

    committed

---
+++ python/Untitled-1.py
@@ -0,0 +1,19 @@
+def isValid(s):
+  bracket_map = {"(": ")",
+                 "[": "]", 
+                 "{": "}"}
+
+  open_par = set(["(", "[", "{"])
+
+  stack = []
+
+  for i in s:
+    if i in open_par:
+        stack.append(i)
+    elif stack and i == bracket_map[stack[-1]]:
+        stack.pop()
+    else:
+        return False
+  return stack == []
+​comb = input("""Write a string that contains only '(', ')', '{', '}', '[' and ']': """)
+print(isValid(comb))
